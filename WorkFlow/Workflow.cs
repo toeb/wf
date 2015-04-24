1e7304371feb0b030776ef520c9769adf2c4dd6a
Tobias Becker <becker.tobi@gmai.com>
2015-04-24 09:51:10 +0200

    initial files

---
+++ WorkFlow/Workflow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFlow
+{
+  public class Workflow
+  {
+  }
+
+
+  public class Activity
+  {
+    
+  }
+}
